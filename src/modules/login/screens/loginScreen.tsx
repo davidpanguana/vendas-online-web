89b165092b3d5c23c0fc26870d208da158dedf1f
davidpanguana <davidpanguana49@gmail.com>
2026-01-11 23:42:29 +0200

    add the logo in login interface

--- src/modules/login/screens/loginScreen.tsx
+++ src/modules/login/screens/loginScreen.tsx
@@ -1,9 +1,15 @@
-import {BackgroundImage} from "../styles/loginScreen.styles"
+import {BackgroundImage, LoginConteiner, LogoImage,LimitedConteiner} from "../styles/loginScreen.styles"
 
 const LoginScreen = () => {
   return (
     <div>
       <BackgroundImage src="./background.png"/>
+      <LoginConteiner>
+        <LimitedConteiner>
+          <LogoImage src="./logo.png"/>
+        </LimitedConteiner>
+      </LoginConteiner>
+
     </div>
   );
 }
--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -8,3 +8,25 @@ export const BackgroundImage = styled.img`
     height:100vh;
     object-fit: cover;
 `;
+
+export const LogoImage = styled.img``;
+
+export const LoginConteiner = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+   background-color: #d9d9d9;
+    width: 100%;
+    height: 100vh;
+    max-width: 646px;
+    position: absolute;
+    right: 0;
+    top: 0;
+`;
+
+export const LimitedConteiner = styled.div`
+    width: 80%;
+    position: relative;
+    background-color: #f0f;
+`;
